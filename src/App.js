752fe374d3c4e0e8315489b2f816954c81e6bfc7
Alvaro Scarafia <alvaro_s.d@hotmail.com>
2022-08-24 11:39:07 -0300

    Agregando componente NavBar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavBar from './componentes/NavBar';
+
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+    );
+}
+
+export default App;
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,20 @@
+import '../App.css'
+import foto from '../multimedia/img/LOGOdisaing.svg';
+
+const NavBar = () => {
+return (
+<div className='container-nav'>
+    <div className='logo'><a href=""><img src={foto} alt="logo"/></a></div>
+        <div>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Galeria</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Ingresar</a></li>
+            </ul>
+        </div>
+</div>
+);
+}
+
+export default NavBar;
