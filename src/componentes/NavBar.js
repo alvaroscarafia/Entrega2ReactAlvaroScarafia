2b5caaf30d7e008e7289bb1c6f4495aee42cc3e1
Alvaro Scarafia <alvaro_s.d@hotmail.com>
2022-08-29 18:26:02 -0300

    Cambiando Paleta de colores y Logo

--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,20 +1,21 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './navbar.css'
-import foto from '../multimedia/img/LOGOdisaing.svg';
+import foto from '../multimedia/img/LOGO.png';
 import CartWidget from './CartWidget.js';
 
 
 const NavBar = () => {
 return (
 <div className='container-nav'>
-    <div className='logo'><a href=""><img src={foto} alt="logo"/></a></div>
+    <div className='logo'><a href=""><img style={{width:500, height:185}} src={foto} alt="logo"/></a></div>
         <div>
             <ul>
                 <li><a href="#">Inicio</a></li>
                 <li><a href="#">Galeria</a></li>
                 <li><a href="#">Productos</a></li>
                 <li><a href="#">Ingresar</a></li>
-                <CartWidget/>
+                <li><a href="#"><CartWidget/></a></li>
             </ul>
         </div>
 </div>
